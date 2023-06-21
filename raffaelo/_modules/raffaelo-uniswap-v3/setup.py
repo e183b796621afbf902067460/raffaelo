0bb23435a7e3045457ca9868a8e9b4be45e1ebb3
e183b796621afbf902067460 <606d18446a06fe9738fd@gmail.com>
2023-06-21 21:19:13 +0000

    Added UniSwapV3PoolContract to raffaelo-uniswap-v3

---
+++ raffaelo/_modules/raffaelo-uniswap-v3/setup.py
@@ -0,0 +1,19 @@
+from setuptools import setup, find_packages
+
+
+with open('README.md', 'r') as readme:
+    long_description = readme.read()
+
+with open('requirements.txt') as requirements:
+    required = requirements.read().splitlines()
+
+setup(
+    name="raffaelo-uniswap-v3",
+    version="0.0.1",
+    author="e183b796621afbf902067460",
+    author_email="606d18446a06fe9738fd@gmail.com",
+    url="https://github.com/e183b796621afbf902067460/raffaelo/",
+    packages=find_packages(),
+    long_description=long_description,
+    install_requires=required,
+)
