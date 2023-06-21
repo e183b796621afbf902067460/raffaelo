da806f83ce731ec3ed5737d38123ce46b7ae36bf
e183b796621afbf902067460 <606d18446a06fe9738fd@gmail.com>
2023-06-21 22:07:10 +0000

    Updated setup in raffaelo-uniswap-v3

--- _modules/raffaelo-uniswap-v3/setup.py
+++ _modules/raffaelo-uniswap-v3/setup.py
@@ -12,7 +12,7 @@ setup(
     version="0.0.1",
     author="e183b796621afbf902067460",
     author_email="606d18446a06fe9738fd@gmail.com",
-    url="https://github.com/e183b796621afbf902067460/raffaelo/tree/master/raffaelo/_modules/raffaelo-uniswap-v3",
+    url="https://github.com/e183b796621afbf902067460/raffaelo/tree/master/_modules/raffaelo-uniswap-v3",
     packages=find_packages(),
     long_description=long_description,
     install_requires=required,
