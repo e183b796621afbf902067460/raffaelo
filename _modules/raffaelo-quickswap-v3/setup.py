808d320c4d44075cc7dbbbea586fa8688652828b
e183b796621afbf902067460 <606d18446a06fe9738fd@gmail.com>
2023-06-25 20:41:54 +0000

    Added setup

---
+++ _modules/raffaelo-quickswap-v3/setup.py
@@ -0,0 +1,21 @@
+from setuptools import setup, find_packages
+
+
+with open('README.md', 'r') as readme:
+    long_description = readme.read()
+
+with open('requirements.txt') as requirements:
+    required = requirements.read().splitlines()
+
+setup(
+    name="raffaelo-quickswap-v3",
+    version="0.0.1",
+    author="e183b796621afbf902067460",
+    author_email="606d18446a06fe9738fd@gmail.com",
+    url="https://github.com/e183b796621afbf902067460/raffaelo/tree/master/_modules/raffaelo-quickswap-v3",
+    packages=find_packages(
+        exclude=['raffaelo_quickswap_v3_tests*']
+    ),
+    long_description=long_description,
+    install_requires=required
+)
