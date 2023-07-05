350bf7871fcd04ccf42162e5624506615035b1b8
e183b796621afbf902067460 <606d18446a06fe9738fd@gmail.com>
2023-07-05 17:43:05 +0000

    Removed raffaelo-blockchain-api module

--- _modules/raffaelo-blockchain-api/raffaelo_blockchain_api/polygon/blockchain.py
+++
@@ -1,9 +0,0 @@
-from raffaelo_blockchain_api.ethereum.blockchain import EthereumEVM
-
-
-class PolygonEVM(EthereumEVM):
-
-    _API_ENDPOINT = 'https://api.polygonscan.com/api'
-
-    def __str__(self) -> str:
-        return __class__.__name__
