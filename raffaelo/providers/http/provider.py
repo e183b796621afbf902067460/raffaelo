e474671739257e19094ada9118a403183c619d42
e183b796621afbf902067460 <606d18446a06fe9738fd@gmail.com>
2023-12-15 07:40:36 +0100

    Updated HTTPProvider

--- raffaelo/providers/http/provider.py
+++ raffaelo/providers/http/provider.py
@@ -2,5 +2,18 @@ from raffaelo.interfaces.providers.interface import iCBP
 
 
 class HTTPProvider(iCBP):
+    """HTTPProvider is a concrete implementation of the iCBP interface for HTTP-based blockchain providers.
+
+    Attributes
+    ----------
+        _uri (str): The URI of the HTTP blockchain provider.
+    """
+
     def __init__(self, uri: str) -> None:
+        """Initializes the HTTPProvider instance with the provided URI.
+
+        Args:
+        ----
+            uri (str): The URI of the HTTP blockchain provider.
+        """
         super().__init__(protocol="http", uri=uri)
