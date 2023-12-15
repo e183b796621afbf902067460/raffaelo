b2c3a12b9e1ff16ee237cb7357075fb453f5c3b5
e183b796621afbf902067460 <606d18446a06fe9738fd@gmail.com>
2023-12-15 20:39:10 +0100

    Updated unit-tests

--- tests/unit/contracts/conftest.py
+++ tests/unit/contracts/conftest.py
@@ -4,6 +4,11 @@ from raffaelo.contracts.erc20.contract import ERC20TokenContract
 from raffaelo.providers.http.provider import HTTPProvider
 
 
+@pytest.fixture(scope="package")
+def provider() -> HTTPProvider:  # noqa: D103
+    return HTTPProvider(uri="https://rpc.ankr.com/eth")
+
+
 @pytest.fixture(scope="module")
 def contract(provider: HTTPProvider) -> ERC20TokenContract:  # noqa: D103
     return ERC20TokenContract(address="0xA0b86991c6218b36c1d19D4a2e9Eb0cE3606eB48", provider=provider)
