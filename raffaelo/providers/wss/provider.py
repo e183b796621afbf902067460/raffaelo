0c33358826308b95cc2463fb9528c27bb1aee2af
e183b796621afbf902067460 <606d18446a06fe9738fd@gmail.com>
2023-12-15 07:40:50 +0100

    Updated WSSProvider

--- raffaelo/providers/wss/provider.py
+++ raffaelo/providers/wss/provider.py
@@ -2,5 +2,18 @@ from raffaelo.interfaces.providers.interface import iCBP
 
 
 class WSSProvider(iCBP):
+    """WSSProvider is a concrete implementation of the iCBP interface for WebSocket-based blockchain providers.
+
+    Attributes
+    ----------
+        _uri (str): The URI of the WebSocket blockchain provider.
+    """
+
     def __init__(self, uri: str) -> None:
+        """Initializes the WSSProvider instance with the provided URI.
+
+        Args:
+        ----
+            uri (str): The URI of the WebSocket blockchain provider.
+        """
         super().__init__(protocol="wss", uri=uri)
