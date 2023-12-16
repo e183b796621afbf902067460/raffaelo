7c54c2ad4fafb4cf5913329d29f530ff86ed8c48
e183b796621afbf902067460 <606d18446a06fe9738fd@gmail.com>
2023-12-17 00:51:37 +0100

    Added test_total_supply to erc20 tests

---
+++ tests/unit/contracts/erc20/test_total_supply.py
@@ -0,0 +1,16 @@
+import pytest
+
+from raffaelo.contracts.erc20.contract import ERC20TokenContract
+
+
+@pytest.fixture(scope="module")
+def total_supply(contract: ERC20TokenContract) -> int:  # noqa: D103
+    yield contract.totalSupply()
+
+
+def test__total_supply__must_be_int(total_supply: int):  # noqa: D103
+    assert isinstance(total_supply, int)
+
+
+def test__total_supply__must_be_positive(total_supply):  # noqa: D103
+    assert total_supply >= 0
